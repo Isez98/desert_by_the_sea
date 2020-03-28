044aa409779acb903bfb73564bc447ed7cb7d7a6
Isacc Hernandez <isacc1798@gmail.com>
2020-03-27 17:46:36 -0700

    Cleaned Code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './designer.css';
+
+//React-Router
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 //Components
 import MenuItem from './Components/MenuItem';
@@ -10,26 +16,57 @@ import MenuList from './Components/MenuList';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">              
-      <img class="logo"  src={require('./Resources/desert_by_the_sea_logo.png')}/> 
-    <MenuList>
-        <MenuItem text='HOME'></MenuItem>        
-        <MenuItem text='CONDOS'></MenuItem>        
-        <MenuItem text='ABOUT US'></MenuItem>
-        <MenuItem text='CONTACT'></MenuItem>
-    </MenuList>
-    <div class= "Main Body">
-        <div>
-            <img class="mainPicture"  src={require('./Resources/Penasco.jpg')}/> 
-            <h3 class="greeting homeText">Welcome to Desert By The Sea's Website!</h3>
-            <p class="paragraph homeText">
-                Desert By The Sea Rentals is a condominium rental and managment company. We offer quality condos at accessible prices. 
-                Our installations at Princesa De Peñasco have pool and beach access, along with a nearby restaurant/bar. We offer both single and
-                double bedrooms along with bathrooms.
-            </p>
-        </div>
+      <Router>
+        <header className="App-header">
+          <img 
+            className="logo" 
+            src={require('./Resources/desert_by_the_sea_logo.png')}
+            alt="Logo of Desert By The Sea" />
+
+          <MenuList>
+            <MenuItem text='Home'></MenuItem>
+            <MenuItem text='Condos'></MenuItem>
+            <MenuItem text='About'></MenuItem>
+            <MenuItem text='Contact'></MenuItem>
+          </MenuList>
+        </header>
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/condos">
+            <Condos />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-      </header>
+  );
+}
+
+function Home() {
+  return (
+    <div class="Main Body">
+      <div>
+        <img 
+            class="mainPicture" 
+            src={require('./Resources/Penasco.jpg')} 
+            alt="Home Page Capture Of Rocky Point"/>
+        <h3 class="greeting homeText">Welcome to Desert By The Sea's Website!</h3>
+        <p class="paragraph homeText">
+          Desert By The Sea Rentals is a condominium rental and managment company. We offer quality condos at accessible prices.
+          Our installations at Princesa De Peñasco have pool and beach access, along with a nearby restaurant/bar. We offer both single and
+          double bedrooms along with bathrooms.
+            </p>
+      </div>
+    </div>
+  );
+}
+
+function Condos() {
+  return (
+    <div class="Main Body">
+      <h1>Condos</h1>
+      <h3>Dos Habitaciones, Dos Baños</h3>
     </div>
   );
 }
--- src/Components/MenuItem/index.js
+++ src/Components/MenuItem/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MenuItem = ({text}) => {
     return (
-        <div class = "travelBar">            
-             {text}
+        <div class = "travelBar" >            
+           <Link to={"/"+{text}}  >{text}</Link>  
          </div >
     )
  }
