aefff702a7c3025ce2ddec84bc304dcae74bc7e7
Isacc Hernandez <isacc1798@gmail.com>
2022-09-04 18:00:08 -0700

    [MOD] Fixed rerendering issue

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,2 +1,35 @@
-import App from './App';
-export default App;
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Footer from '../Components/Footer'
+import Nav from '../Components/Nav'
+import ContextWrapper from '../Context/ContextWrapper'
+import Home from '../Paginas/Home'
+
+const App: React.FC = () => {
+  return (
+    <ContextWrapper>
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <Nav />
+            <Switch>
+              {
+                //  <Route path="/condos">
+                //    <Paginas.Condos hash={pageLang} />
+                //  </Route>
+              }
+              <Route path="/">
+                {' '}
+                <Home />{' '}
+              </Route>
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ContextWrapper>
+  )
+}
+
+export default App
--- src/Components/DetectLang/index.ts
+++ src/Components/DetectLang/index.ts
@@ -1,24 +1,12 @@
 function DetectLang() {
-  const navLang = navigator.language.substr(0, 2)
-
-  let detectLang = function (name: string, url: string) {
-    if (!url) url = window.location.href
-    name = name.replace(/[[\]]/g, '\\#')
-    var regex = new RegExp('[#]' + name + '(=([^&#]*)|&|#|$)'),
-      results = regex.exec(url)
-    if (!results) return null
-    if (!results[2]) return ''
-    return decodeURIComponent(results[2].replace(/\+/g, ' '))
+  const navLang = navigator.language.substring(0, 2)
+  switch (navLang) {
+    case 'en':
+      return 'en'
+    case 'es':
+      return 'es'
+    default:
+      return 'en'
   }
-
-  let lang = function () {
-    if (detectLang('lang', '') === null) {
-      return navLang
-    }
-    return detectLang('lang', '')
-  }
-
-  let detectedLang = lang()
-  return detectedLang
 }
 export default DetectLang
